06ee16a25e187e00c4dc71bbe623afc2a8af7154
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-03-14 18:07:53 +0530

    Update chap3.js

--- chap3.js
+++ chap3.js
@@ -25,3 +25,38 @@ function findPattern(n){
 }
 
 findPattern(13);
+
+
+/*Write a function to find the minimum of 2 args*/
+
+function minimum(a,b){
+  return a < b ? a : b;
+}
+
+
+/*
+Zero is even.
+• One is odd.
+• For any other number N, its evenness is the same as N - 2.
+Define a recursive function isEven corresponding to this description. The
+function should accept a single parameter (a positive, whole number) and return
+a Boolean.
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a
+way to fix this?
+*/
+
+function isEven(n){
+  if(n < 0)
+    n = -n;
+  
+  if(n == 0)
+    return true;
+  if (n == 1)
+    return false;
+  
+  else if (n > 1)
+    return isEven(n-2);  
+}
+
+
+
