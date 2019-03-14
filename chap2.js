ee8239601fa27bf75234b167bb9ff6598061b4bf
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-03-14 13:41:12 +0530

    Update chap2.js

--- chap2.js
+++ chap2.js
@@ -12,6 +12,7 @@ triangle:
 Answer:
 */
 
+/*Attempt 1*/
 for (let i = 0; i<7; i++){
  let j = '';
  for (let k = 0; k < i+1; k++){
@@ -19,4 +20,17 @@ for (let i = 0; i<7; i++){
  }
  j += '\n';
  console.log(j); 
+}
+
+/*Attempt 2*/
+
+var acc = '', total = (7 * (7+1))/2 ;
+let target = 1, rowsCount = 1;
+for (let i = 1; i <= total; i++){
+  acc = acc + '#';
+  if( i === target){
+     acc = acc + '\n';
+     rowsCount++;  
+     target = target + rowsCount;
+  }
 }
