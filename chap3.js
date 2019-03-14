458d2074da4410987df49a75a7bf77f3cf5bfcec
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-03-14 18:15:59 +0530

    Update chap3.js

--- chap3.js
+++ chap3.js
@@ -59,4 +59,27 @@ function isEven(n){
 }
 
 
+/*
+Write a function countBs that takes a string as its only argument and returns
+a number that indicates how many uppercase “B” characters there are in the
+string.
+Next, write a function called countChar that behaves like countBs, except
+it takes a second argument that indicates the character that is to be counted
+(rather than counting only uppercase “B” characters). Rewrite countBs to
+make use of this new function.
+*/
+
+function countChars(str, character){
+  let counter = 0;
+  for(let i=0; i < str.length; i++){
+    if(str[i] === character)
+      counter++;
+  }
+  return counter;
+}
+
+function countBs(str){
+  return countChars(str, 'B')
+}
+
 
