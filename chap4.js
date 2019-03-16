5a226c52258a47d83e9cb71df7745d49516f6b1c
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-03-16 13:30:36 +0530

    Update chap4.js

--- chap4.js
+++ chap4.js
@@ -138,5 +138,39 @@ function nth(list, index){
 }
 
 
+/*
+Write a function deepEqual that takes two values and returns true only if they
+are the same value or are objects with the same properties, where the values
+of the properties are equal when compared with a recursive call to deepEqual.
+To find out whether values should be compared directly (use the === operator
+for that) or have their properties compared, you can use the typeof operator.
+If it produces "object" for both values, you should do a deep comparison.
+But you have to take one silly exception into account: because of a historical
+accident, typeof null also produces "object".
+The Object.keys function will be useful when you need to go over the properties
+of objects to compare them.
+*/
 
 
+function deepEqual(v1,v2){
+  if(typeof v1 !== typeof v2)
+    return false;
+  else {
+    if(typeof v1 !== 'object' || (v1 == null || v2 == null))
+      return v1 === v2;
+    else {
+      if(Object.keys(v1).length !== Object.keys(v2).length)
+        return false;
+      else {
+        let i, isSame = true;
+        for(i in v1){
+          if(!(i in v2))
+            return false;
+          else 
+            isSame = isSame & deepEqual(v1[i],v2[i]);
+        }
+        return isSame;
+      }
+    }
+  }
+}
