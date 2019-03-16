85ca912eb5fce3bd6899ff79c5ed2041acb22923
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-03-16 10:17:33 +0530

    Update weresquirrel.js

--- weresquirrel.js
+++ weresquirrel.js
@@ -3,8 +3,7 @@ This file contains all the code for weresquirrel example in this book. Althogh t
 for better understanding, I am trying to write the functions by myself
 */
 
-function phi(table){
-  let n11 = table[3], n00 = table[0], n01 = table[1], n10 = table[2];
+function phi([n00,n01,n10,n11]){
   let n1$ = n11 + n10;
   let n0$ = n01 + n00;
   let n$1 = n01 + n11;
