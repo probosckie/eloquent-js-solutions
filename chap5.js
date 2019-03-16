b656b77bd5ad1c315e70ab354074988c658ac356
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-03-16 18:31:45 +0530

    Update chap5.js

--- chap5.js
+++ chap5.js
@@ -47,6 +47,22 @@ function flatten(arr){
   }, [])
 }
 
+/*
+Write a higher-order function loop that provides something like a for loop
+statement. It takes a value, a test function, an update function, and a body
+function. Each iteration, it first runs the test function on the current loop value
+and stops if that returns false. Then it calls the body function, giving it the
+current value. Finally, it calls the update function to create a new value and
+starts from the beginning.
+*/
 
+
+function loop(v,test,update,body){
+  let current = v;
+  while(test(current)){
+    body(current);
+    current = update(current);
+  }
+}
 
 
