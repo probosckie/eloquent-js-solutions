84436a55630fa473667e5f3b1258987a060d9108
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-03-17 15:02:44 +0530

    Update matrixClass.js

--- matrixClass.js
+++ matrixClass.js
@@ -25,3 +25,38 @@ class Matrix {
     this.content[this.width*y + x] = value;
   }
 }
+
+
+class MatrixIterator {
+  constructor(matrix){
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+  
+  next(){
+    if(this.y == this.matrix.height && this.x == this.matrix.width)
+      return {done:true};
+    let value = {
+      x: this.x,
+      y: this.y,
+      value: this.matrix.get(this.x,this.y)
+    }
+    this.x++;
+    if(this.x == this.matrix.width){
+      this.x = 0;
+      this.y++;
+    }
+    return {
+      value, done: false
+    };
+  }
+}
+
+Matrix.prototype[Symbol.iterator] = function(){
+  return new MatrixIterator(this);
+}
+
+let matrix = new Matrix(2,2, (x,y) => `Value ${x} ${y}`);
+
+
