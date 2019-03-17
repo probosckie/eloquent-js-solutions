0955864d4d7fdab4df238d77c0427b308621d587
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-03-17 11:32:49 +0530

    Update chap6.js

--- chap6.js
+++ chap6.js
@@ -7,3 +7,51 @@ function speak(line){
 }
 
 const whiteRabbit = {type:'white', speak};
+
+/*
+Passing explicit value of this via call
+*/
+
+speak.call(whiteRabbit, 'Something in the way she moves');
+
+/*
+Arrow functions are different—they do not bind their own this but can see
+the this binding of the scope around them. Thus, you can do something like
+the following code, which references this from inside a local function:
+*/
+
+function normalize(){
+  console.log(`Normalized co-ordinates ${this.coords.map(v => v/this.length)}`);
+}
+
+normalize.call({coords:[0,2,3], length:5})
+
+/*
+Object.getPrototypeOf method gives prototype of a given object, 
+Object.prototype is the root ancestral object of the empty object sitting at the top of the chain
+*/
+
+Object.getPrototypeOf({}) === Object.prototype
+
+/*
+You can use Object.create to create an object with a specific prototype
+*/
+
+const x = {a:1}
+const y = Object.create(x);
+y.newProp = 34;
+
+y.a //will give 1
+
+/*
+Shorthand to create methods inside objects
+*/
+
+const a = {
+  tellMe(line){
+    console.log(line)
+  }
+};
+
+a.tellMe('Something in the way she moves');
+
