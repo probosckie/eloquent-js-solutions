7dc1ebb2f06a76d7e31bfcd3653c8cffd864a09b
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-03-17 18:04:17 +0530

    Update chap6.js

--- chap6.js
+++ chap6.js
@@ -162,6 +162,36 @@ class Temperature{
   }
 }
 
+/*
+Write a class Vec that represents a vector in two-dimensional space. It takes
+x and y parameters (numbers), which it should save to properties of the same
+name.
+Give the Vec prototype two methods, plus and minus, that take another
+vector as a parameter and return a new vector that has the sum or difference
+of the two vectors’ (this and the parameter) x and y values.
+Add a getter property length to the prototype that computes the length of
+the vector—that is, the distance of the point (x, y) from the origin (0, 0).
+*/
+
+class Vec {
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+  
+  plus(v){
+    return new Vec(this.x + v.x, this.y + v.y);
+  }
+  
+  minus(v){
+    return new Vec(this.x - v.x, this.y - v.y);
+  }
+  
+  get length(){
+     return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+}
+
 
 
 
