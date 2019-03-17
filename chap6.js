4381a4e0a5054f3c9834c4a872ed0c1d0dea51f9
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-03-17 19:12:33 +0530

    Update chap6.js

--- chap6.js
+++ chap6.js
@@ -238,6 +238,49 @@ class Group {
   }
 }
 
+/*
+Make the Group class from the previous exercise iterable. Refer to the section
+about the iterator interface earlier in the chapter if you aren’t clear on the
+exact form of the interface anymore.
+If you used an array to represent the group’s members, don’t just return the
+iterator created by calling the Symbol.iterator method on the array. That
+would work, but it defeats the purpose of this exercise.
+It is okay if your iterator behaves strangely when the group is modified during
+iteration.
+*/
+
+
+class GroupIterator {
+  constructor(group){
+    this.currentIndex = 0;
+    this.group = group;
+  }
+  next(){
+    if(this.currentIndex === this.group.length)
+      return {done: true};
+    else {
+      let value = {
+        value: this.group.bucket[this.currentIndex];
+      };
+      this.currentIndex++;
+      return {
+        value,
+        done: false
+      }
+    }
+  }
+}
+
+Group.prototype[Symbol.iterator] = function(){
+  return new GroupIterator(this);
+}
+
+
+
+
+
+
+
 
 
 
