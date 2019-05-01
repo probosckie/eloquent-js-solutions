cb26132c905fe798c9377c028d774c88a2461ce3
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-05-01 16:21:13 +0530

    Update dsAlgoProbs.js

--- dsAlgoProbs.js
+++ dsAlgoProbs.js
@@ -10,12 +10,13 @@ var prices = [1,3,4,3,89,2,15,7,8,9,2,3];
 
 /* brute force */
 
-let buyIndex = 0; sellIndex = 1;
+let buyIndex, sellIndex;
 let maxDiff = -Infinity;
 
 for (let i = 0; i < prices.length - 1; i++){
 	for(let j = i + 1; j < prices.length; j++){
 		if((prices[j] - prices[i]) > maxDiff){
+			maxDiff = prices[j] - prices[i];
 			buyIndex = i;
 			sellIndex = j;
 		}
