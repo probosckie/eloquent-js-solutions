fc8ab1a3e4eb0e3ed3d182b1b57d2c2ed065616e
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-05-04 16:43:46 +0530

    Update dsAlgoProbs.js

--- dsAlgoProbs.js
+++ dsAlgoProbs.js
@@ -25,3 +25,38 @@ for (let i = 0; i < prices.length - 1; i++){
 
 console.log(`Gonna buy on ${buyIndex} and sell on ${sellIndex}`);
 console.log(`Gonna buy at ${prices[buyIndex]} and sell at ${prices[sellIndex]}`);
+
+/* To understand divide and conquer, we will first write mergesort */
+
+let arr = [1,3,4,1,22,89,-1,0];
+
+function mergeSort(a){
+	function sortAndMerge(a1,a2){
+    let newA = [];
+    a1 = !!a1 ? a1.slice() : [];
+    a2 = !!a2 ? a2.slice() : [];
+    while(a1.length + a2.length){
+      if(a1.length && (!a2.length || a1[0] <= a2[0])){
+        newA.push(a1.shift());
+      }
+      if(a2.length && (!a1.length || a2[0] <= a1[0])){
+        newA.push(a2.shift())
+      }	
+    }
+	return newA;
+	}
+	
+	function divide(arr){
+		if(arr.length > 1){
+			let arr1 = arr.slice(0, arr.length/2);
+			let arr2 = arr.slice(arr.length/2, arr.length);
+			return sortAndMerge(divide(arr1), divide(arr2));
+		}
+		if(arr.length === 1){
+			return arr;
+		}
+	}
+	return divide(a);
+}
+
+mergeSort(arr);
